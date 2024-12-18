207e680610948b355081f220ab6013a7f5c45dbd
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-18 20:29:34 +0300

    Validate title

--- models/article.py
+++ models/article.py
@@ -1,5 +1,12 @@
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
+        if not isinstance(title, str):
+            raise ValueError("Title must be a string")
+        
+        if len(title) < 5 or len(title) > 50:
+            raise ValueError("Title must be between 5 and 50 characters, inclusive")
+        
+        
         self.id = id
         self.title = title
         self.content = content
