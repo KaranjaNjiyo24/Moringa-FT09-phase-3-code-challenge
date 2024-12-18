d93bccf10d044c1bfccd6a8612f71683e0400f59
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-18 21:14:46 +0300

    Connect to database to insert or retrieve magazine

--- models/magazine.py
+++ models/magazine.py
@@ -1,3 +1,4 @@
+from database.connection import get_db_connection
 class Magazine:
     def __init__(self, id, name, category):
         if not isinstance(name, str):
@@ -10,7 +11,28 @@ class Magazine:
         if len(category.strip()) == 0:
             raise ValueError("Category must be longer than 0 characters")
         
+        conn = get_db_connection()
+        cursor = conn.cursor()
         
+        try:
+            if id is None:
+                cursor.execute('INSERT INTO magazines (name, category) VALUES (?, ?)', (name, category))
+                conn.commit()
+                self._id = cursor.lastrowid
+            else:
+                cursor.execute('SELECT * FROM magazines WHERE id = ?', (id,))
+                if not cursor.fetchone():
+                    raise ValueError(f"No magazine found with id {id}")
+                self._id = id
+            
+            self._name = name
+            self._category = category
+        except Exception as e:
+            conn.rollback()
+            raise ValueError(f"Error creating/retrieving magazine: {e}")
+        finally:
+            conn.close()
+
         self.id = id
         self.name = name
         self.category = category
