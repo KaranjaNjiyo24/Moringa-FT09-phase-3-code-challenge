35fcdb7c3f9abfc9da81b811052ac41ef3324a2e
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-18 19:06:11 +0300

    Create an entry in the datatbase

--- models/author.py
+++ models/author.py
@@ -1,14 +1,36 @@
+from database.connection import get_db_connection
 class Author:
     def __init__(self, id, name):
+        self.id = id
+        self.name =name
+        
         if not isinstance(name, str):
             raise ValueError("Name must be a string")
         
         if len(name.strip()) == 0:
             raise ValueError("Name must be a string")
         
-        
-        self.id = id
-        self.name = name
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        try:
+            if id is None:
+                cursor.execute('INSERT INTO authors (name) VALUES (?)', (name,))
+                conn.commit()
+                self._id = cursor.lastrowid
+            else:
+                cursor.execute('SELECT * FROM authors WHERE id = ?', (id,))
+                if not cursor.fetchone():
+                    raise ValueError(f"No author found with id {id}")
+                self._id = id
+
+            self._name = name
+        except Exception as e:
+            conn.rollback()
+            raise ValueError(f"Error creating/retrieving author: {e}")
+        finally:
+            conn.close()
+
 
     def __repr__(self):
         return f'<Author {self.name}>'
