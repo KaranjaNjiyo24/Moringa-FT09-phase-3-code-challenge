fe4850c10be999c8bfe1dd18b5106916ddd188f1
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-18 18:48:30 +0300

    validate name

--- models/author.py
+++ models/author.py
@@ -1,5 +1,12 @@
 class Author:
     def __init__(self, id, name):
+        if not isinstance(name, str):
+            raise ValueError("Name must be a string")
+        
+        if len(name.strip()) == 0:
+            raise ValueError("Name must be a string")
+        
+        
         self.id = id
         self.name = name
 
