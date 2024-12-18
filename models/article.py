c81e8a02c3466caa7da68b7f160a0d219ceb8819
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-18 20:41:28 +0300

    Connect to database to retrieve article

--- models/article.py
+++ models/article.py
@@ -1,3 +1,5 @@
+from database.connection import get_db_connection
+
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
         if not isinstance(title, str):
@@ -6,12 +8,33 @@ class Article:
         if len(title) < 5 or len(title) > 50:
             raise ValueError("Title must be between 5 and 50 characters, inclusive")
         
-        
-        self.id = id
-        self.title = title
-        self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
+        conn = get_db_connection
+        cursor = conn.cursor
+
+        try:
+            if id is None:
+                cursor.execute('''
+                    INSERT INTO articles (title, content, author_id, magazine_id)
+                    VALUES (?,?,?,?)
+                    ''', (title, content, author_id, magazine_id))
+                conn.commit()
+                self._id = cursor.lastrowid
+            else:
+                cursor.execute('SELECT * FROM articles WHERE id= ?', (id,))
+                if not cursor.fetchone():
+                    raise ValueError(f"No article found with id {id}")
+                self._id =id
+
+            self._title = title
+            self._content = content
+            self._author_id = author_id
+            self._magazine_id = magazine_id
+        except Exception as e:
+            conn.rollback()
+            raise ValueError(f"Error creating/retrieving article: {e}")
+        finally:
+            conn.close()
+    
 
     def __repr__(self):
         return f'<Article {self.title}>'
