ffd4bed94b23c639eaa609dcb970341f3c44395c
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-18 21:12:32 +0300

    validate name and category

--- models/magazine.py
+++ models/magazine.py
@@ -1,5 +1,16 @@
 class Magazine:
     def __init__(self, id, name, category):
+        if not isinstance(name, str):
+            raise ValueError("Name must be a string")
+        if len(name) < 2 or len(name) > 16:
+            raise ValueError("Name must be between 2 and 16 characters, inclusive")
+        
+        if not isinstance(category, str):
+            raise ValueError("Category must be a string")
+        if len(category.strip()) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        
+        
         self.id = id
         self.name = name
         self.category = category
